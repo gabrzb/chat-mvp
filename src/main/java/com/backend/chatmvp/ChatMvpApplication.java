57ae6d255acbe408f5794a46550c65f0c9428a16
gabrzb <gabrielzbz.gz@gmail.com>
2026-06-11 19:56:31 -0300

    fix: minor ajustments for first commit

--- src/main/java/com/backend/chatmvp/ChatMvpApplication.java
+++ src/main/java/com/backend/chatmvp/ChatMvpApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChatMvpApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(ChatMvpApplication.class, args);
     }
 
